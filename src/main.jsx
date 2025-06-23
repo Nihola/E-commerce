feb210349ad67a5b961a1a30a3550a9da35737e6
Nihola <niholaadbugopirova@gmail.com>
2025-06-24 03:23:47 +0500

    Merge is completed

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import {RouterProvider} from "react-router-dom" //add*
 import './index.css'
 import App from './App.jsx'
 import { Router } from './routers/Router.jsx'//add*
-import { RouterProvider } from 'react-router-dom'
-import { Router } from './routers/Router';
+ 
+ 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
