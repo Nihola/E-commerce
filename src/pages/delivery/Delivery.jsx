7eac4895f8bcf2764c5a982d6c9dab319d37bc97
sirojiddin0505 <sirojiddintohirjanov6@gmail.com>
2025-06-23 22:22:23 +0500

    installed: npm and tailwindcss

--- src/pages/delivery/Delivery.jsx
+++ src/pages/delivery/Delivery.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import WhereDelivery from './WhereDelivery'
+import EnterDelivery from './EnterDelivery'
 
 export default function Delivery() {
   return (
     <section>
-      <div></div>
+      <div >
+         {/* left div  */}
+         <div>
+          <WhereDelivery/>
+          <WhereDelivery/>
+          <EnterDelivery/>
+         </div>
+
+
+         {/* right div  */}
+         <div>
+
+         </div>
+      </div>
     </section>
   )
 }
---
+++ src/pages/delivery/WhenDelivery.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const WhenDelivery = () => {
+  return (
+    <div>WhenDelivery</div>
+  )
+}
+
+export default WhenDelivery
