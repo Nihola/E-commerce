347509cf43bd06b3e0750bb306df9680b246ef72
sirojiddin0505 <sirojiddintohirjanov6@gmail.com>
2025-06-24 09:58:31 +0500

    commit

--- src/pages/delivery/Delivery.jsx
+++ src/pages/delivery/Delivery.jsx
@@ -1,22 +1,19 @@
 import React from 'react'
 import WhereDelivery from './WhereDelivery'
 import EnterDelivery from './EnterDelivery'
+import SumDelivery from './SumDelivery'
 
 export default function Delivery() {
   return (
     <section>
-      <div >
-         {/* left div  */}
+      <div className='container mx-auto flex justify-between gap-8 items-center'>
          <div>
           <WhereDelivery/>
           <WhereDelivery/>
           <EnterDelivery/>
          </div>
-
-
-         {/* right div  */}
          <div>
-
+          <SumDelivery/>
          </div>
       </div>
     </section>
