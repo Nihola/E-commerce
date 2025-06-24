9db8f54eb0fd1e9bd9be5156c87a5e12a8005adf
Absalamova <zilolaxonabsalomova@gmail.com>
2025-06-24 22:51:24 +0500

    add footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import { FaInstagram, FaVk, FaFacebookF, FaOdnoklassniki, FaPhoneAlt } from "react-icons/fa";
+
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+     <footer className="bg-[#fdf6e8] w-full text-gray-700 text-sm py-4 border-t border-gray-200 " >
+      <div className="max-w-7xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center gap-4">
+        <div className="flex flex-wrap justify-center gap-4 ">
+          <Link className='hover:text-orange-400'>О компании</Link>
+          <Link className='hover:text-orange-400' >Контакты</Link>
+          <Link className='hover:text-orange-400' >Вакансии</Link>
+          <Link className='hover:text-orange-400' >Статьи</Link>
+          <Link className='hover:text-orange-400' >Политика обработки персональных данных</Link>
+        </div>
+        <div className="flex items-center gap-4">
+          <FaInstagram className="text-xl hover:text-orange-400" />
+          <FaVk className="text-xl  hover:text-orange-400" />
+          <FaFacebookF className="text-xl hover:text-orange-400" />
+          <FaOdnoklassniki className="text-xl  hover:text-orange-400" />
+        </div>
+        <div className="flex items-center gap-2  hover:text-orange-400">
+          <FaPhoneAlt /> <span>8 800 777 33 33</span>
+        </div>
+      </div>
+    </footer>
   )
 }
+
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { FaHeart, FaBox, FaShoppingCart, FaSearch, FaBars } from "react-icons/fa";
 import Logo from "./logo.svg"
@@ -13,19 +14,21 @@ export default function Header() {
   };
   return (
     <div className="w-full flex items-center justify-between bg-white p-2 shadow-md">
-      {/* Logo va katalog */}
+      
       <div className="flex items-center gap-2">
-        <div className="flex items-center">
-          <img src={Logo} alt="logo" />
-          <div className="ml-1 font-semibold text-sm">СЕВЕРЯНОЧКА</div>
-        </div>
+       <Link to="/">
+          <div className="flex items-center">
+            <img src={Logo} alt="logo" />
+            <div className="ml-1 font-semibold text-sm">СЕВЕРЯНОЧКА</div>
+          </div>
+       </Link>
+
         <button className="flex items-center bg-green-500 text-white px-4 py-2 rounded-lg">
           <FaBars className="mr-2" />
           Каталог
         </button>
       </div>
 
-      {/* Qidiruv */}
       <div className="flex items-center flex-1 mx-4">
         <input
           type="text"
@@ -37,34 +40,36 @@ export default function Header() {
         </button>
       </div>
 
-      {/* Icons va profil */}
+    
       <div className="flex items-center gap-4">
         <div
           onClick={goToFavorites}
-          className="relative flex flex-col items-center text-sm text-gray-700 cursor-pointer hover:text-red-500 transition"
-        >
-           
+          className="relative flex flex-col items-center text-sm text-gray-700 cursor-pointer hover:text-red-500 transition">
           <FaHeart className="text-xl mb-1" />
- 
           {favorites.length > 0 && (
             <span className="absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 bg-red-500 text-white text-[10px] font-bold px-1.5 py-[2px] rounded-full">
               {favorites.length}
             </span>
           )}
-
-          {/* Label */}
           <div className="text-sm text-center">Избранное</div>
         </div>
 
-        <div className="flex flex-col items-center text-sm text-gray-700">
-          <FaBox className="text-xl" />
-          <div className="text-sm text-center">Заказы </div>
-        </div>
-        <div className="relative">
-          <FaShoppingCart className="text-2xl text-gray-700" />
-          <div className="absolute -top-2 right-[12px] bg-red-500 text-white text-xs rounded-full px-1">0</div>
-          <div className="text-sm text-center">Корзина</div>
-        </div>
+        <Link to="/delivery">
+          <div className="flex flex-col items-center text-sm text-gray-700 cursor-pointer hover:text-red-500 transition">
+            <FaBox className="text-xl" />
+            <div className="text-sm text-center">Заказы </div>
+          </div>
+        </Link>
+
+        <Link to="/cart">
+          <div className="relative cursor-pointer hover:text-red-500 transition">
+            <FaShoppingCart className="text-2xl text-gray-700 cursor-pointer hover:text-red-500 transition" />
+            <div className="absolute -top-2 right-[12px] bg-red-500 text-white text-xs rounded-full px-1">
+              0
+            </div>
+            <div className="text-sm text-center">Корзина</div>
+          </div>
+        </Link>
         <div className="flex items-center gap-2">
             <img
               src={Avatar}
