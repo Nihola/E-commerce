bbffd6299fefe3d747704e1708a3fbf624a3b58e
Nihola <niholaadbugopirova@gmail.com>
2025-06-24 13:18:41 +0500

    working on the design of Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,16 @@
  
 import { products } from './../../products-data';
 import ProductSection from './../../components/ProductSection';
-
+import ProductCard from "./../../components/ProductCard";
 const promotions = products.filter(p => p.tag === "Promotions");
 const newArrivals = products.filter(p => p.tag === "New Arrivals");
 const previouslyBought = products.filter(p => p.tag === "Previously Bought");
 
 function HomePage() {
   return (
-    <div>
-      <ProductSection title="🔥 Promotions" data={promotions} />
+    <div className='bg-[#FFF3E9]'>
+
+      <ProductSection title="🔥 Promotions" data={promotions}/>
       <ProductSection title="🆕 New Arrivals" data={newArrivals} />
       <ProductSection title="🛍️ Previously Bought" data={previouslyBought} />
     </div>
