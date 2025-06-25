8d0dcd58ed9180fac17df330be7039b207fd80db
sirojiddin0505 <sirojiddintohirjanov6@gmail.com>
2025-06-25 11:57:26 +0500

    Add:telegram bot

--- src/pages/delivery/Delivery.jsx
+++ src/pages/delivery/Delivery.jsx
@@ -1,22 +1,59 @@
 import React from 'react'
+import { useForm } from 'react-hook-form'
 import WhereDelivery from './WhereDelivery'
+import WhenDelivery from './WhenDelivery'
 import EnterDelivery from './EnterDelivery'
 import SumDelivery from './SumDelivery'
-import WhenDelivery from './WhenDelivery'
 
 export default function Delivery() {
+  const { register, handleSubmit, reset } = useForm()
+  console.log("Register nima:", register)
+  const sendMessage = async (data) => {
+    const message = `
+    🛒 <b>Yangi buyurtma</b>:
+    📍 <b>Hudud:</b> ${data.district}
+    🏘 <b>Ko'cha:</b> ${data.street}, Uy: ${data.home}, Kvartira: ${data.apartment}
+    📝 <b>Qo'shimcha:</b> ${data.additional || 'yo‘q'}
+    📅 <b>Sana:</b> ${data.date}
+    ⏰ <b>Vaqt:</b> ${data.times}
+    📞 <b>Tel:</b> ${data.phone}
+    `
+    const TOKEN = '8171242828:AAGekrDGyZJMZdghTIZurMr78LZpCBTQZiQ'
+    const CHAT_ID = '1456386212'
+    const URL = `https://api.telegram.org/bot${TOKEN}/sendMessage`
+
+    try {
+      await fetch(URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          chat_id: CHAT_ID,
+          text: message,
+          parse_mode: 'HTML'
+        })
+      })
+      alert('Buyurtma yuborildi ✅')
+      reset()
+    } catch (error) {
+      alert('Xatolik yuz berdi!')
+      console.error(error)
+    }
+  }
   return (
-    <section className='bg-[#f2eddc] p-6 '>
-      <div className='container mx-auto flex justify-between gap-8 items-center'>
-         <div className='flex flex-col gap-8 w-[600px]'>
-          <WhereDelivery/>
-          <WhenDelivery/>
-          <EnterDelivery/>
-         </div>
-         <div>
-          <SumDelivery/>
-         </div>
-      </div>
+    <section className='bg-[#f2eddc] p-6'>
+      <form onSubmit={handleSubmit(sendMessage)} className='max-w-[1000px] p-4 mx-auto flex justify-between gap-12 items-start'>
+        <div className='flex flex-col gap-8 w-[600px]'>
+          <WhereDelivery register={register} />
+          <WhenDelivery register={register} />
+          <EnterDelivery register={register} />
+          <button type='submit' className='bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700'>
+            Buyurtma berish
+          </button>
+        </div>
+        <div>
+          <SumDelivery />
+        </div>
+      </form>
     </section>
   )
 }
--- src/pages/delivery/SumDelivery.jsx
+++ src/pages/delivery/SumDelivery.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SumDelivery = () => {
+  const [isOn, setIsOn] = useState(false)
   return (
-    <section>
-        
+    <section className=' w-[300px]'>
+      <div className='flex items-center gap-4 py-8'>
+        <div onClick={() => setIsOn(!isOn)} className={`w-14 h-8 flex items-center bg-gray-300 rounded-full p-1 cursor-pointer ${isOn ? 'bg-green-500' : ''}`}><div className={`bg-white w-6 h-6 rounded-full shadow-md transform ${isOn ? 'translate-x-6' : ''}`}/>
+        </div>
+        <p className='text-[#414141] text-xl'>200$ Chegirma</p>
+      </div>
+      <div className='py-4 space-y-2'>
+        <span className='flex justify-between gap-4'>
+          <p className='text-xl text-[#8F8F8F]'>3 ta mahsulot</p>
+          <p className='text-xl text-[#414141] font-bold'>258$</p>
+        </span>
+        <span className='flex justify-between gap-4'>
+          <p className='text-xl text-[#8F8F8F]'>Chegirma</p>
+          <p className='text-xl text-[#FF6633] font-bold'>-8$</p>
+        </span>
+      </div>
+      <div className='flex justify-between items-center gap-4 py-8'>
+        <h2 className='text-2xl text-[#2f2d2d]'>Umumiy Summa</h2>
+        <p className='text-2xl font-bold text-[#414141]'>250$</p>
+      </div>
+      <div>
+        <button className='w-full border p-3 bg-orange-500 text-white rounded-lg font-semibold cursor-pointer hover:bg-orange-400 active:scale-96 duration-250 mb-2'>Oldindan to'lov</button>
+        <button className='w-full border p-3 bg-green-500 text-white rounded-lg font-semibold cursor-pointer hover:bg-green-400 active:scale-96 duration-250'>Yetkazib bergandan keyin to'lov</button>
+      </div>
+
     </section>
   )
 }
