52099abff47cefa8bb7e0a5fae5c79a673fc710a
Azizbek18 <https://github.com/Azizbek18/responsiv-project.git>
2025-06-27 13:53:29 +0500

    Add responsive

--- src/pages/singlepage/SinglePage.jsx
+++ src/pages/singlepage/SinglePage.jsx
@@ -110,8 +110,8 @@ return (
         </button>
       </div>
     </div>
-    <div className=' flex flex-col lg:flex-row gap-8 '>
-      <div className="w-full flex flex-col lg:flex-row gap-5 lg:h-[400px]">
+    <div className=' flex flex-col lg:flex-row gap-0.5 lg:gap-8 '>
+      <div className="w-full flex flex-col lg:flex-row gap-5   lg:h-[400px]">
         <div className="flex lg:flex-col sm:gap-1.5 lg:gap-2 justify-between w-full  lg:w-[120px]">
           {singleCard.images.slice(0, 4).map((img, index) => (
           <img key={index} src={img} alt={`img-${index}`} onClick={()=> handleClick(img)}
@@ -132,7 +132,7 @@ return (
         </div>
       </div>
 
-      <div className="w-full lg:w-1/2 flex flex-col gap-6 ">
+      <div className="w-full lg:w-1/2 flex flex-col gap-1 ">
         <div className="">
           <div className="flex items-center w-full  justify-between">
             <div className="flex w-[140px] flex-col ">
