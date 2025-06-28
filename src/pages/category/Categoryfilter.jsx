494edbddc01a97abbbca83f9c78bda202d7cdb91
Raymov <googlaccnew12@gmil.com>
2025-06-28 19:07:16 +0500

    new update :update category 28/06/25 19:10

--- src/pages/category/Categoryfilter.jsx
+++ src/pages/category/Categoryfilter.jsx
@@ -188,7 +188,7 @@ setProduct(filtered);}
 </>}
       
       {/* Список продуктов */}
-      <main className="flex-1 h-screen lg:mt-15">
+      <main className="flex-1 h-[90vw] lg:mt-15">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-6">
           {visibleProducts.map((product) => (
             <ProductCard key={product.id} product={product} />
